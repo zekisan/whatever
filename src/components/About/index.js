aa5f7cf78593b3a5f907509cb33a71e57587a0ac
Ezequiel <zeki-san@hotmail.com>
2017-09-07 21:31:07 -0300

    withProps

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link }from 'react-router-dom';
 import { Container, Card, CardText, CardBlock, CardTitle, CardSubtitle, Button } from 'reactstrap';
-import { withHandlers } from 'recompose';
+import { compose, withHandlers, withProps } from 'recompose';
 
 import  * as AboutActions from '../../stores/reducers/About';
 import './style.css';
 
 const About = (props) => {
-    const { number, onClick } = props;
+    const { totalNumber, onClick } = props;
     return (
         <div className='About'>
             <p><Link to="/">Home</Link></p>
             <h1>Fuck the king 2 - Abouts</h1>
-            <h1>Counter - { number }</h1>
+            <h1>Counter - { totalNumber }</h1>
             <Container>
                 <Card block>
                     <CardBlock>
@@ -29,14 +29,22 @@ const About = (props) => {
 }
 
 About.propTypes = {
-    //about: PropTypes.object,
     number: PropTypes.number
 };
 
-const decorate = withHandlers({
+const withComponentHandlers = withHandlers({
     onClick: ({ store }) => (e) => {
         store.dispatch(AboutActions.addNumber());
     }
 });
+
+const withComponentProps = withProps(props => ({
+    totalNumber: props.number + 1
+}));
+
+const decorate = compose(
+    withComponentProps,
+    withComponentHandlers
+);
 
 export default decorate(About);
