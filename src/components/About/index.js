1bc3c49263161093db64b32ced2e4c82d5f724a4
Ezequiel <zeki-san@hotmail.com>
2017-09-09 19:53:29 -0300

    sem redux!!!

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,56 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link }from 'react-router-dom';
 import { Container, Card, CardText, CardBlock, CardTitle, CardSubtitle, Button } from 'reactstrap';
-import { compose, withHandlers, withProps } from 'recompose';
 import { withRouter } from 'react-router-dom'
 
-import  * as AboutActions from '../../stores/reducers/About';
 import './style.css';
 
-const About = (props) => {
-    const { totalNumber, onClick, onPush } = props;
-    return (
-        <div className='About'>
-            <p><Link to="/">Home</Link></p>
-            <h1>Fuck the king 2 - Abouts</h1>
-            <h1>Counter - { totalNumber }</h1>
-            <Container>
-                <Card block>
-                    <CardBlock>
-                        <CardTitle>Card title</CardTitle>
-                        <CardSubtitle>Card subtitle</CardSubtitle>
-                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                        <Button color="danger" onClick={onClick}>Button</Button>
-                    </CardBlock>
-                </Card>
-            </Container>
-            <Button color="danger" onClick={onPush}>Redirect nowhere</Button>
-        </div>
-    );
+class About extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onPush = this.onPush.bind(this);
+    }
+
+    onPush(e){
+        this.props.history.push('/asdlkajsd');
+    }
+
+    render() {
+        const { onClick, number } = this.props;
+        return (
+            <div className='About'>
+                <p><Link to="/">Home</Link></p>
+                <h1>Fuck the king 2 - Abouts</h1>
+                <h1>Counter - { number }</h1>
+                <Container>
+                    <Card block>
+                        <CardBlock>
+                            <CardTitle>Card title</CardTitle>
+                            <CardSubtitle>Card subtitle</CardSubtitle>
+                            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                            <Button color="danger" onClick={onClick}>Button</Button>
+                        </CardBlock>
+                    </Card>
+                </Container>
+                <Button color="danger" onClick={this.onPush}>Redirect nowhere</Button>
+            </div>
+        );
+    }
 }
 
+export default withRouter(About);
+
 About.propTypes = {
-    number: PropTypes.number
+    number: PropTypes.number,
+    history: PropTypes.object,
+    onClick: PropTypes.func
 };
-
-const withComponentHandlers = withHandlers({
-    onClick: ({ store }) => (e) => {
-        store.dispatch(AboutActions.addNumber());
-    },
-    onPush: ({ history }) => (e) => {
-        history.push('/asdlkajsd');
-    }
-});
-
-const withComponentProps = withProps(props => ({
-    totalNumber: props.number + 1
-}));
-
-const decorate = compose(
-    withComponentProps,
-    withRouter,
-    withComponentHandlers
-);
-
-export default decorate(About);
--- src/routes.js
+++ src/routes.js
@@ -1,31 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Provider, connect } from 'react-redux';
 
 import App from './components/App';
 import About from './components/About';
 import NotFound from './components/NotFound';
 
-const Routes = (props) => {
-    return (
-        <Provider store={props.store}>
+const reducer = function reducer(state = { number: 0 }, action = {}) {
+    switch(action.type) {
+        case 'ADD_ONE':
+            return { number: state.number + 1 }
+        default:
+            return state;
+    }
+}
+
+const dispatch = function dispatch(currentState, action) {
+    return reducer(currentState, action);
+}
+
+class Routes extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onClick = this.onClick.bind(this);
+        this.state = dispatch(undefined, {});
+    }
+
+    onClick() {
+        this.setState(
+            dispatch(this.state, { type: 'ADD_ONE' })
+        );
+    }
+
+    render() {
+        const { title } = this.props;
+        const { number } = this.state;
+        return (
             <Router>
                 <Switch>
-                    <Route exact path="/"><App title={props.title}/></Route>
-                    <Route path="/about"><About store={props.store} number={props.about.number}/></Route>
+                    <Route exact path="/"><App title={title}/></Route>
+                    <Route path="/about">
+                        <About number={number} onClick={this.onClick}/>
+                    </Route>
                     <Route path="*" component={NotFound}/>
                 </Switch>
             </Router>
-        </Provider>
-    );
+        );
+    }
 }
 
-function mapStateToProps(state) {
-    return { about: state.AboutReducer.toJS() }
-}
-
-const ConnectedComponent = connect(mapStateToProps)(Routes);
-
-export default function Root(props) {
-    return <Provider store={props.store}><ConnectedComponent {...props} /></Provider>
-}
+export default Routes;
